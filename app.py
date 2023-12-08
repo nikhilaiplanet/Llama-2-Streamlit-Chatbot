b24d2f95c96cb0ccff2f4c2138437cde193d6910
nikhilchintawar <chintawarnikhil88@gmail.com>
2023-12-08 16:00:45 +0530

    Add hf model & query

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 import streamlit as st
 from streamlit_chat import message
-import tempfile   # temporary file
+import tempfile  # temporary file
 from langchain.document_loaders.csv_loader import CSVLoader  # using CSV loaders
-from langchain.embeddings import HuggingFaceEmbeddings # import hf embedding
+from langchain.embeddings import HuggingFaceEmbeddings  # import hf embedding
 from langchain.vectorstores import FAISS
 from langchain.llms import CTransformers
 from langchain.chains import ConversationalRetrievalChain
+from langchain.llms.huggingface_hub import HuggingFaceHub
+from dotenv import load_dotenv
+import os
 
-DB_FAISS_PATH = 'vectorstore/db_faiss' # # Set the path of our generated embeddings
+DB_FAISS_PATH = "vectorstore/db_faiss"  # # Set the path of our generated embeddings
+
+load_dotenv()
+huggingface_key = os.getenv("HUGGINGFACE_KEY")
 
 
 # Loading the model of your choice
 def load_llm():
     # Load the locally downloaded model here
-    llm = CTransformers(
-        model="llama-2-7b-chat.ggmlv3.q8_0.bin",
-        model_type="llama",
-        max_new_tokens=512,
-        temperature=0.5
+    # llm = CTransformers(
+    #     model="llama-2-7b-chat.ggmlv3.q8_0.bin",
+    #     model_type="llama",
+    #     max_new_tokens=512,
+    #     temperature=0.5,
+    # )
+
+    llm = HuggingFaceHub(
+        repo_id="HuggingFaceH4/zephyr-7b-beta",
+        model_kwargs={"temperature": 0.5, "max_length": 64, "max_new_tokens": 512},
+        huggingfacehub_api_token=huggingface_key,
     )
+
     # the model defined, can be replaced with any ... vicuna,alpaca etc
     # name of model
     # tokens
@@ -26,27 +39,34 @@ def load_llm():
     return llm
 
 
-st.title("Llama2 Chat CSV - 🦜🦙")
-#st.markdown("<h3 style='text-align: center; color: white;'>Built by <a href=''></a></h3>",unsafe_allow_html=True)
+st.title("Robo Advisor - 🦜🦙")
+# st.markdown("<h3 style='text-align: center; color: white;'>Built by <a href=''></a></h3>",unsafe_allow_html=True)
 
-uploaded_file = st.sidebar.file_uploader("Upload File", type="csv") # uploaded file is stored here
+uploaded_file = st.sidebar.file_uploader(
+    "Upload File", type="csv"
+)  # uploaded file is stored here
 # file uploader
 if uploaded_file:
     # tempfile needed as CSVLoader accepts file_path exclusively
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name # save file locally
+        tmp_file_path = tmp_file.name  # save file locally
 
-    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','}) # any loader can be put here based on the data being used
+    loader = CSVLoader(
+        file_path=tmp_file_path, encoding="utf-8", csv_args={"delimiter": ","}
+    )  # any loader can be put here based on the data being used
     # csv_args={'delimiter': ','} for faulty formatted csv
-    data = loader.load() # load the data
-    #st.json(data)   # uncomment to check uploaded data
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',model_kwargs={'device': 'cpu'}) # use sentence transformer to create embeddings
+    data = loader.load()  # load the data
+    # st.json(data)   # uncomment to check uploaded data
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+    )  # use sentence transformer to create embeddings
 
     # FAISS Can be replaced by Chroma... so it will be like CHROMA.fromdocuments...
-    db = FAISS.from_documents(data, embeddings) # pass data embeddings vector data here
-    db.save_local(DB_FAISS_PATH) # save vector embedding here on mentioned path
-    llm = load_llm() # Load the Language model here
+    db = FAISS.from_documents(data, embeddings)  # pass data embeddings vector data here
+    db.save_local(DB_FAISS_PATH)  # save vector embedding here on mentioned path
+    llm = load_llm()  # Load the Language model here
 
     # the conversational chain which preserves context learning in chat
     chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
@@ -55,26 +75,32 @@ if uploaded_file:
     # func for streamlit chat takes query from User
     def conversational_chat(query):
         # key value pairs of conversational history
-        result = chain({"question": query, "chat_history": st.session_state['history']}) # enduser query and result variable
+        result = chain(
+            {
+                "question": query
+                + "You're a AI financial adviser who answers questions based on given context if question is out of context then just say I don't know.",
+                "chat_history": st.session_state["history"],
+            }
+        )  # enduser query and result variable
 
         # add all responses here with query to preserve context
-        st.session_state['history'].append((query, result["answer"]))
-        return result["answer"] # get the generated result
-
+        st.session_state["history"].append((query, result["answer"]))
+        return result["answer"]  # get the generated result
 
     # appending history
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
+    if "history" not in st.session_state:
+        st.session_state["history"] = []
 
     # Start message, in context of no question having being not asked yet
     if 'generated' not in st.session_state:
         st.session_state['generated'] = ["Hello ! Ask me(LLAMA2) about " + uploaded_file.name + " 🤗"]
 
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
+
+    if "past" not in st.session_state:
+        st.session_state["past"] = ["Hey ! 👋"]
 
     # container for the chat history
-    response_container = st.container() # form
+    response_container = st.container()  # form
 
     # container for the user's text input
     container = st.container()
@@ -87,16 +113,18 @@ if uploaded_file:
         if submit_button and user_input:
             output = conversational_chat(user_input)
 
-            st.session_state['past'].append(user_input) # old user input is appended
-            st.session_state['generated'].append(output) # append the generated
+            st.session_state["past"].append(user_input)  # old user input is appended
+            st.session_state["generated"].append(output)  # append the generated
 
-    if st.session_state['generated']:
+    if st.session_state["generated"]:
         with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
-
-
-
-
-
+            for i in range(len(st.session_state["generated"])):
+                message(
+                    st.session_state["past"][i],
+                    is_user=True,
+                    key=str(i) + "_user",
+                    avatar_style="big-smile",
+                )
+                message(
+                    st.session_state["generated"][i], key=str(i), avatar_style="thumbs"
+                )
